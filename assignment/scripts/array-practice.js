9a93a183455b704a173338a4d2638e12a499d36b
Todd McCoy <tcmccoy@gmail.com>
2022-05-18 10:03:35 -0500

    completed stretch questions

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -84,16 +84,25 @@ console.log('My favorite foods are now', favFoods);
 // 4.e (STRETCH) TODO: Replace the second food in your array
 //      with another one of your favorite foods.
 //      Then log the updated array.
+favFoods[1] = 'pulled pork';
+console.log(favFoods);
 
 // 4.f (STRETCH) TODO: Sort your favoriteFoods array
 //     in reverse alphabetical order. Log the array.
+favFoods.sort();
+favFoods.reverse();
+console.log(favFoods);
 
 // 4.g (STRETCH) TODO: Convert your array to a string
 //     putting the word "and" between each item.
 //     eg "tacos and pizza and pasta". Log the string.
+let foodString = favFoods.join(" and ");
+console.log(foodString);
 
 // 4.h (STRETCH) TODO: Make a new array that combines 
 //     the favorite foods array with the animals array.
 //     Then log the new array.
 //     It should look something like:
 //     ['pizza', 'pasta', 'fish', 'cat', 'bird', 'dog']
+let animalFood = favFoods.concat(animalArray);
+console.log(animalFood);
